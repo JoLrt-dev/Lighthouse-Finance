c6067fe6e66169275e00fd39d1308059ce9ae8f1
JoLrt-dev <j.laurent1993@gmail.com>
2026-02-12 19:15:08 +0100

    Logger

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-require("dotenv").config(); // Charge les variables du fichier .env
-const port = process.env.APP_PORT || 3000; // Utilise le port du .env, ou 3000 par défaut
+import dotenv from "dotenv";
+import express from "express";
+import { cyanBright, cyan } from "colorette";
+
+const app = express();
+const port = process.env.APP_PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Le serveur est en ligne et il répond !");
+});
+
+// Logger Start
+app.listen(port, () => {
+  console.log(cyanBright("##################################"));
+  console.log(cyan(`# Server is running on port ${port} #`));
+  console.log(cyanBright("##################################"));
+});
+
+process.on("uncaughtException", (error) => {
+  console.error(error);
+  process.exit(1);
+});
+// Logger End
